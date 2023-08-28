50e9f11f2a19ab5e5173b25c8c0940b586386beb
Ismael <ismaelcosta949@gmail.com>
2023-08-28 18:53:40 -0300

    first commit

---
+++ src/main/java/com/example/Demo_SalaF/DemoSalaFApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demo_SalaF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSalaFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSalaFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Demo_SalaF/DemoSalaFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo_SalaF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSalaFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
